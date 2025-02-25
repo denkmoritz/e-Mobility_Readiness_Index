d266ac6177a820d4539285cd8f14b51c8f971d72
denkmoritz <moritzdenk0407@gmail.com>
2025-02-25 16:16:10 +0100

    update process files

--- process_files.py
+++ process_files.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
-files_lk = ['ladesaeuleninfrastruktur_alle_LANDKREISE_2020-2021.csv']
+files_lk = ['strom-aus-erneuerbaren-quellen_alle_LANDKREISE_2020-2021.csv']
 
-column_names = ["Name", "unknown_1", "unknown_2", "Bundesland","2020", "2021"]
-columns_to_remove = ["unknown_1", "unknown_2"]
+column_names = ["Name", "ID", "unknown_2", "Bundesland", "2020", "2021"]
+columns_to_remove = ["unknown_2"]
 
 for file in files_lk:
-    df = pd.read_csv(file, sep=';', skiprows=4, skipfooter=10, engine='python', header=None, names=column_names)
+    df = pd.read_csv(file, sep=';', skiprows=4, skipfooter=10, engine='python', header=None, names=column_names, dtype={"ID": str})
+
+    # Remove unwanted columns
     df = df.drop(columns=columns_to_remove)
-    df.to_csv("ladesaeulen_landkreise.csv", sep=';', index=False)
+
+    # Keep only the first six characters of the 'ID' column
+    df["ID"] = df["ID"].str[:6]
+
+    # Save the cleaned dataframe
+    df.to_csv("strom_landkreise.csv", sep=';', index=False)
